6811cebe46b985fd8a0699cf00c3123f0bbab353
Mustkeem K <mustkeem@upstack.co>
2022-08-19 18:44:09 +0530

    add swagger api docs

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const fs = require("fs");
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -34,23 +35,22 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -60,17 +60,28 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
+// import library and files
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const customCss = fs.readFileSync(process.cwd() + "/swagger.css", "utf8");
+// let express to use this
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { customCss })
+);
+
 //mongodb+srv://ship:ship12345@cluster0-r3cso.mongodb.net/blog-post-demo?retryWrites=true&w=majority
 //
-const port  = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 mongoose
   .connect(
-    'mongodb+srv://ship:ship12345@cluster0-r3cso.mongodb.net/blog-post-demo?retryWrites=true&w=majority',
+    "mongodb+srv://ship:ship12345@cluster0-r3cso.mongodb.net/blog-post-demo?retryWrites=true&w=majority",
     { useNewUrlParser: true }
   )
-  .then(result => {
-    app.listen(port, ()=>{
-      console.log("**********Running on***********", port)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log("**********Running on***********", port);
     });
   })
-  .catch(err => console.log("********Error in connection********",err));
+  .catch((err) => console.log("********Error in connection********", err));
